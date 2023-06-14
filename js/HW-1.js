7fdedc426463d12200c91a98aaed745a63a9915e
AigerimAdr <aigerim210596@gmail.com>
2023-06-14 12:44:19 +0600

    Выполнила ДЗ-2

--- js/HW-1.js
+++ js/HW-1.js
@@ -24,27 +24,60 @@ const childBlock = document.querySelector('.child_block')
 
 let positionX = 0
 let positionY = 0
-let positionZ = 0
-let positionW = 0
 
 const moveBlock = () => {
-    if (positionX < 449) {
-        positionX += 2;
-        childBlock.style.left = `${positionX}px`;
-        setTimeout(moveBlock, 10);
-    } else if (positionY < 449) {
-        positionY += 2;
-        childBlock.style.top = `${positionY}px`;
-        setTimeout(moveBlock, 10);
-    } else if (positionZ < 449) {
-        positionZ += 2;
-        childBlock.style.right = `${positionZ}px`;
-        setTimeout(moveBlock, 10);
-    } else if (positionZ >= 449 && positionW < 449) {
-        positionW += 2;
-        childBlock.style.top = `${positionW}px`;
-        setTimeout(moveBlock, 10);
+    if (positionX <= 449 && positionY <= 0) {
+        positionX += 2
+        childBlock.style.left = `${positionX}px`
+        setTimeout(moveBlock, 10)
+    } else if (positionX >= 449 && positionY <= 449) {
+        positionY += 2
+        childBlock.style.top = `${positionY}px`
+        setTimeout(moveBlock, 10)
+    } else if (positionX >= 0 && positionY >= 449) {
+        positionX -= 2
+        childBlock.style.left = `${positionX}px`
+        setTimeout(moveBlock, 10)
+    } else if (positionX <= 449 && positionY >= 0) {
+        positionY -= 2
+        childBlock.style.top = `${positionY}px`
+        setTimeout(moveBlock, 10)
     }
-};
+}
 
-moveBlock();
+moveBlock()
+
+
+// HW 2 part 1
+
+const startButton = document.querySelector('.start')
+const stopButton = document.querySelector('.stop');
+const resumeButton = document.querySelector('.resume')
+const clearButton = document.querySelector('.clear')
+const counter = document.querySelector('#clock').style.color = 'yellow'
+
+let second = 0;
+let interval;
+
+const start = () => {
+    const begin = () => {
+        second++;
+        clock.innerHTML = second;
+    }
+    begin()
+    interval = setInterval(begin, 1000)
+}
+
+const clear = () => {
+    clearInterval(interval);
+    second = 0;
+    clock.innerHTML = second;
+}
+
+const stop = () => { clearInterval(interval); }
+const resume = () => { start() }
+
+startButton.onclick = () => start()
+stopButton.onclick = () => stop()
+resumeButton.onclick = () => resume()
+clearButton.onclick = () => clear()
